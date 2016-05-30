fe8e9b431afdb0cf3e67ed9fc7300d943520da84
Mingliang Liu <mliu@hortonworks.com>
2016-05-29 23:47:50 -0700

    Always build the base image

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ cd $HADOOP_SRC_HOME
 echo "Building Hadoop...."
 #mvn package -DskipTests -Dtar -Pdist -q || exit 1
 HADOOP_TARGET_SNAPSHOT=$(find $HADOOP_SRC_HOME/hadoop-dist/target/ -type d -name 'hadoop-*-SNAPSHOT')
-echo $HADOOP_TARGET_SNAPSHOT
 cd -
 
 # build Spark
@@ -16,13 +15,12 @@ cd -
 #mvn package -DskipTests -Dtar -Pdist -q || exit 1
 #cd -
 
-docker rmi -f hadoop-and-spark-on-docker
+docker build -t hadoop-and-spark-on-docker-base .
 
 docker build -t "hadoop-and-spark-on-docker" - << EOF
 FROM hadoop-and-spark-on-docker-base
 
 ENV HADOOP_HOME /usr/lib/hadoop
-
 ADD $HADOOP_TARGET_SNAPSHOT \$HADOOP_HOME
 ADD files/* \$HADOOP_HOME/etc/hadoop/conf
 EOF
