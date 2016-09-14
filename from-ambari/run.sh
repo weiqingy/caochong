3632e3ef373132f5d377b8d65f1ad7620027b9c1
Mingliang Liu <liuml07@apache.org>
2016-09-14 13:24:38 -0700

    Print user-friendly the container info

--- from-ambari/run.sh
+++ from-ambari/run.sh
@@ -7,22 +7,28 @@ docker build -t caochong-ambari .
 docker network create caochong 2> /dev/null
 
 # remove the outdated master
-docker rm -f $(docker ps -a -q -f "name=$NODE_NAME_PREFIX") 2> /dev/null
-# launch master container
+docker rm -f $(docker ps -a -q -f "name=$NODE_NAME_PREFIX") 2>&1 > /dev/null
+
+# launch containers
 master_id=$(docker run -d --net caochong -p 8080:8080 --name $NODE_NAME_PREFIX-0 caochong-ambari)
 echo ${master_id:0:12} > hosts
 for i in $(seq $((N-1)));
 do
-    docker run -d --net caochong --name $NODE_NAME_PREFIX-$i caochong-ambari
+    container_id=$(docker run -d --net caochong --name $NODE_NAME_PREFIX-$i caochong-ambari)
     echo ${container_id:0:12} >> hosts
 done
 
+# Copy the workers file to the master container
+docker cp hosts $master_id:/root
 # print the hostnames
 echo "Using the following hostnames:"
+echo "------------------------------"
 cat hosts
-# print the private key
-echo "Using the following private key to ssh:"
-docker exec -it $NODE_NAME_PREFIX-0 cat /root/.ssh/id_rsa
+echo "------------------------------"
 
-# Connect to the master node
-docker exec -it $NODE_NAME_PREFIX-0 /bin/bash
+# print the private key
+echo "Copying back the private key..."
+docker cp $master_id:/root/.ssh/id_rsa .
+
+# Start the ambari server
+docker exec $NODE_NAME_PREFIX-0 ambari-server start
